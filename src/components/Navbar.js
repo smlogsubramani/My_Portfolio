839f829b610b9d133a5e22cdd3e329cc09849b30
smlogsubramani <smlogasubramani@gmail.com>
2022-10-23 23:25:36 +0530

    npm build error rectification

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,7 +95,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/smlogsubramani/Portfolio"
+                href="https://github.com/smlogsubramani/My_Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
